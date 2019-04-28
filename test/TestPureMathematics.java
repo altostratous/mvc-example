0e235af3a4f62bfe04966f3e2d1397b7a1ad6934
altostratous <altostratous@gmail.com>
2019-04-28 19:15:09 +0430

    add test

---
+++ test/TestPureMathematics.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by HP PC on 4/28/2019.
+ * Project MVC
+ */
+public class TestPureMathematics {
+    @Test
+    public void testMathematics() {
+        TestCase.assertEquals(1 + 1, 2);
+    }
+}
